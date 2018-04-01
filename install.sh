8a35554be4ec30082fd18a058d19d505bdd33fd7
alexander.russell@sap.com <alexander.russell@sap.com>
2018-04-01 18:07:38 +0200

    Install and configure motion.  Use nginx with rtmp module built from source.

--- install.sh
+++ install.sh
@@ -1,38 +1,58 @@
 #!/bin/bash
 
+FN_REPO_URL=https://github.com/alexjrussell/fruitnanny
+NGINX_VERSION=1.10.3
+
 # Check we are root
 if [ "$(whoami)" != "root" ]; then
     echo "You must be root to run this script"
     exit 1
 fi
 
-# TODO: Run raspi config first
-# - Make sure the camera is enabled
-# Run rpi-update
+# Check the distribution and version
+RECOMMENDED_DIST=0
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    if [ "$ID" == "raspbian" -a "$VERSION_ID" == "9" ]; then
+        RECOMMENDED_DIST=1
+    fi
+fi
+if [ "$RECOMMENDED_DIST" == "0" ]; then
+    echo "WARNING - You are not using the the recommended distribution or version"
+    echo "This script has only been tested with Raspbian 9 (stretch)"
+    echo "Press enter to continue or Ctrl-C to abort"
+    read
+fi
 
+echo "You need to have met the following pre-requisites before running this script:"
+echo "- Enabled the camera"
+echo "- Enlarged the root partition"
+
+# Disable wifi power-save mode
+echo "Disable wifi power save mode"
+iw dev wlan0 set power_save off
+echo "wireless-power off" >> /etc/network/interfaces
+
+# Disable ipv6
+echo "Disable ipv6"
+cat >> /etc/modprobe.d/ipv6.conf <<EOF
+alias ipv6 off
+options ipv6 disable_ipv6=1
+blacklist ipv6
+EOF
+
+# Update the package list
 apt-get -y update
 
 # Upgrade the Raspberry Pi’s firmware
 apt-get -y install rpi-update
-## TODO: Stop it from asking for confirmation
-rpi-update
+yes | rpi-update
 
 # Install packages
-apt-get -y install vim git nano emacs libraspberrypi-dev autoconf automake libtool pkg-config \
+apt-get -y install vim git nano emacs libraspberrypi-dev autoconf automake libtool pkg-config avahi-daemon\
     alsa-base alsa-tools alsa-utils build-essential python-dev python-pip
 
 pip install xvfbwrapper
-
-# Run raspberry config to enable camera and resize partition (options 1 and 5):
-# TODO
-#raspi-config
-
-# Disable wifi power-save mode
-iw dev wlan0 set power_save off
-echo "wireless-power off" >> /etc/network/interfaces
-
-# Access trough .local domain (for instance pi.local or fruitnanny.local)
-apt-get -y install avahi-daemon
 
 # Download & install nodejs
 curl -sL https://deb.nodesource.com/setup_6.x | bash -
@@ -44,7 +64,7 @@ apt-get -y install python-gst-1.0 python-gobject xvfb pulseaudio dbus-x11
 
 # Install fruitnanny
 cd /opt
-git clone https://github.com/alexjrussell/fruitnanny
+git clone $FN_REPO_URL
 chown -R pi:pi /opt/fruitnanny
 cd /opt/fruitnanny
 sudo -u pi npm install
@@ -82,7 +102,16 @@ make
 make install
 
 # Install motion
-apt-get -y install motion
+dpkg --install https://github.com/Motion-Project/motion/releases/download/release-4.1.1/pi_stretch_motion_4.1.1-1_armhf.deb
+# Install motion config and service
+chown pi:pi /var/lib/motion
+mv /etc/motion/motion.conf /etc/motion/motion.conf_old
+ln -s /opt/fruitnanny/configuration/motion/motion.conf /etc/motion/motion.conf
+update-rc.d motion disable
+ln -s /opt/fruitnanny/configuration/systemd/motion.service /etc/systemd/system/
+systemctl enable motion
+
+#modprobe bcm2835-v4l2
 
 # Install Janus WebRTC Gateway
 apt-get -y install libmicrohttpd-dev libjansson-dev libnice-dev \
@@ -119,13 +148,11 @@ python setup.py install
 ln -s /opt/fruitnanny/configuration/systemd/noise.service /etc/systemd/system/
 ln -s /opt/fruitnanny/configuration/systemd/video.service /etc/systemd/system/
 ln -s /opt/fruitnanny/configuration/systemd/janus.service /etc/systemd/system/
-ln -s /opt/fruitnanny/configuration/systemd/fruitnanny.service /etc/systemd/system/
 
 # Enable services
 systemctl enable noise
 systemctl enable video
 systemctl enable janus
-systemctl enable fruitnanny
 
 # Install nodejs process manager (PM2) for automatic nodejs app startup
 #npm install pm2 -g
@@ -135,29 +162,36 @@ systemctl enable fruitnanny
 # Run this as pi user?
 #sudo -u pi pm2 save
 
-# install nginx
-apt-get -y install nginx
+# Install nginx & rtmp module from source
+apt-get -y install build-essential libpcre3 libpcre3-dev libssl-dev unzip
+wget -O /tmp/nginx.tgz http://nginx.org/download/nginx-$NGINX_VERSION.tar.gz
+cd /opt
+tar -xzf /tmp/nginx.tgz
+wget -O /tmp/nginx-rtmp.zip https://github.com/arut/nginx-rtmp-module/archive/master.zip
+unzip /tmp/nginx-rtmp.zip
+cd /opt/nginx-$NGINX_VERSION
+./configure --with-http_ssl_module --add-module=../nginx-rtmp-module-master
+make
+make install
 
-# Remove default site
-rm -f /etc/nginx/sites-enabled/default
+# Configure the nginx service
+update-rc.d nginx disable
+ln -s /opt/fruitnanny/configuration/systemd/nginx.service /etc/systemd/system/
 
-# Copy fruitnanny configs
-ln -s /opt/fruitnanny/configuration/nginx/fruitnanny_http /etc/nginx/sites-available/fruitnanny_http
-ln -s /opt/fruitnanny/configuration/nginx/fruitnanny_https /etc/nginx/sites-available/fruitnanny_https
-
-# enable new configs
-ln -s /etc/nginx/sites-available/fruitnanny_http /etc/nginx/sites-enabled/
-ln -s /etc/nginx/sites-available/fruitnanny_https /etc/nginx/sites-enabled/
+# Install the nginx config
+mv /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/nginx.conf_old
+ln -s /opt/fruitnanny/configuration/nginx/nginx.conf /usr/local/nginx/conf/nginx.conf
 
 # Configure the password
-sh -c "echo -n 'fruitnanny:' >> /etc/nginx/.htpasswd"
+sh -c "echo -n 'fruitnanny:' >> /usr/local/nginx/conf/.htpasswd"
 echo "Enter password for fruitnanny web application"
-sh -c "openssl passwd -apr1 >> /etc/nginx/.htpasswd"
+sh -c "openssl passwd -apr1 >> /usr/local/nginx/conf/.htpasswd"
+
 systemctl enable nginx
 
+# TODO
 sudo -u pi pm2 start /opt/fruitnanny/server/app.js --name="fruitnanny"
 sleep 5s
 sudo -u pi pm2 save
 sudo -u pi pm2 kill
 echo "Installation successful - Starting Fruitnanny"
-systemctl start fruitnanny
