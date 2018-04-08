7b4fd7eea9c8e74feca7211cdcb339889c826dee
D. L. Marrero <dleefilms@gmail.com>
2018-04-08 02:42:46 +0000

    Add react container to index.html

---
+++ src/__test__/App.spec.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as TestUtils from 'react-dom/test-utils';
+import { shallow } from 'enzyme';
+import App from '../App';
+
+
+describe('App', () => {
+  it('should render without crashing', () => {
+    shallow(<App />);
+  });
+});
