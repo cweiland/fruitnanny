6a395f0df02117e1d6756a610cc796823ed8da07
D. L. Marrero <dleefilms@gmail.com>
2018-04-08 04:15:38 +0000

    Add jest types for improved autocomplete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import React, { Component } from 'react';
+/* eslint-disable react/jsx-filename-extension */
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div />
-    );
-  }
-}
+const App = () => <div />;
 
 export default App;
--- src/__test__/App.spec.jsx
+++ src/__test__/App.spec.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
-import * as TestUtils from "react-dom/test-utils";
+// import * as ReactDOM from "react-dom";
+// import * as TestUtils from "react-dom/test-utils";
 import { shallow } from "enzyme";
 import App from "../App";
 
