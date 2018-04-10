f84c5a1c6be84388b869bf972fba237e8a4872f4
D. L. Marrero <dleefilms@gmail.com>
2018-04-10 23:28:23 +0000

    Add react and its dependencies to project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 /* eslint-disable react/jsx-filename-extension */
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        react works!
-      </div>
-    );
+    return <div />;
   }
 }
 
