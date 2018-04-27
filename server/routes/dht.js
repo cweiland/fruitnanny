9bf16abbff36ae596883ca9e103b865880bceba7
D. L. Marrero <dleefilms@gmail.com>
2018-04-27 00:07:40 +0000

    refactor: clean up temperature and humidity fetch

--- server/routes/dht.js
+++ server/routes/dht.js
@@ -5,11 +5,8 @@ const cp = require("child_process");
 let router = express.Router();
 router.get("/current", (req, res, next) => {
     cp.exec("bin/dht22.py", (err, stdout, stderr) => {
-        let values = stdout.split(" ");
-        let t = values[0];
-        let h = values[1];
-        let result = { humidity: h, temperature: t };
-        res.json(result);
+        let [temperature, humidity] = stdout.split(" ");
+        res.json({ temperature, humidity });
     });
 });
 exports.default = router;
